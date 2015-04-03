8d82c462bd56477c239ca09bbe34f646090972ea
abdullahi796 <abdullahimahamed0987@gmail.com>
2015-04-03 12:14:38 -0500

    Revert "April 4"
    This reverts commit 7c2c874ccd829782eb81752b623850032d74c77c.

--- GridGame/GridGame/Form1.vb
+++ GridGame/GridGame/Form1.vb
@@ -37,7 +37,7 @@ Public Class Form1
         Panel1.Height = Me.Height
         Panel1.Left = 0
         Panel1.Top = 0
-        player = New Ball(3, 3, "Ball_0._Purple_png")
+        player = New Ball(3, 3, "Ball_0.png")
         player.setup()
         Panel1.Controls.Add(player.ball)
         Dim tileReader As String
@@ -61,7 +61,7 @@ Public Class Form1
                 End If
             Next
         Next
-        restart(1)
+        restart(3)
         restartLevel = True
         num = 1
     End Sub
@@ -171,7 +171,7 @@ Public Class Form1
         ElseIf picUpRight.Bounds.Contains(PointToClient(MousePosition)) Then
             mouseImg = "UpRight.png"
         ElseIf picTile.Bounds.Contains(PointToClient(MousePosition)) Then
-            mouseImg = "Tile_0_Blue.png"
+            mouseImg = "block.png"
         End If
     End Sub
 
@@ -234,7 +234,6 @@ Public Class Form1
         Public last As String
         Public lastTile As String
         Public lastCount As Integer
-        Public playerColor As String
 
         Public Sub New(ByVal tempX As Integer, ByVal tempY As Integer, ByVal tempImg As String)
             x = tempX * 60
@@ -244,7 +243,6 @@ Public Class Form1
             img = tempImg
             locX = tempX
             locY = tempY
-            'playerColor = tempColor
         End Sub
         Public Sub setup()
             ball = New Windows.Forms.PictureBox
@@ -259,12 +257,6 @@ Public Class Form1
 
         End Sub
         Public Sub move()
-            'If img = "Blue" Then
-            '    img = "Ball_0_Blue"
-            'ElseIf img = "Purple" Then
-            '    img = "Ball_0_Purple"
-            'End If
-            'ball.Image = Image.FromFile(img)
             If key = "space" Or Form1.restartLevel = True Then
                 Form1.restartLevel = False
                 ball.Top = y - 2
